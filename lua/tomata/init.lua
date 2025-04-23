b6db26c6e9a03856b4c16cec46857bbeea3e957e
Pau Perez <saulopefa@gmail.com>
2025-04-23 21:55:40 +0200

    Use conventional table name

--- lua/tomata/init.lua
+++ lua/tomata/init.lua
@@ -2,42 +2,42 @@ local notify = function(msg, level)
   vim.notify(msg, level or vim.log.levels.INFO, { title = "🍅 Tomata" })
 end
 
-local Tomata = { timer = nil }
+local M = { timer = nil }
 
-function Tomata.start(duration) -- Duration in minutes
-  if Tomata.timer then
-    Tomata.stop()
+function M.start(duration) -- Duration in minutes
+  if M.timer then
+    M.stop()
   end
 
   duration = tonumber(duration) or 25 -- Default to 25 minutes
-  Tomata.timer = vim.uv.new_timer()
+  M.timer = vim.uv.new_timer()
 
-  if not Tomata.timer then
+  if not M.timer then
     notify("Failed to create timer", vim.log.levels.ERROR)
     return
   end
 
-  Tomata.timer:start(duration * 60 * 1000, 0, function()
+  M.timer:start(duration * 60 * 1000, 0, function()
     notify("Time is up!")
-    Tomata.stop()
+    M.stop()
   end)
 
   notify("Tomata started for " .. duration .. " minutes")
 end
 
-function Tomata.stop()
-  if Tomata.timer then
-    Tomata.timer:stop()
-    Tomata.timer:close()
-    Tomata.timer = nil
+function M.stop()
+  if M.timer then
+    M.timer:stop()
+    M.timer:close()
+    M.timer = nil
   end
 end
 
 vim.api.nvim_create_user_command("Tomata", function(opts)
   if opts.bang then
-    Tomata.stop()
+    M.stop()
     notify("Tomata stopped")
     return
   end
-  Tomata.start(opts.args)
+  M.start(opts.args)
 end, { nargs = "?", bang = true })
