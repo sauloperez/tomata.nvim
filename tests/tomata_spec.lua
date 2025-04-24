b0a670d66104e9ef8690b4c1b3dfd4b27a297fd8
Pau Perez <saulopefa@gmail.com>
2025-04-24 09:26:06 +0200

    Test pluralization

--- tests/tomata_spec.lua
+++ tests/tomata_spec.lua
@@ -22,6 +22,19 @@ describe("Tomata", function()
     assert(find_message("Starting pomodoro timer for 1 minute"), "Expected start message not found")
   end)
 
+  it("pluralizes the output message", function()
+    tomata.setup({ duration = 2 })
+    tomata.start()
+
+    local timer = tomata.pomodoro_timer
+    if not timer then
+      error("Timer is nil")
+    end
+
+    eq(timer:is_active(), true)
+    assert(find_message("Starting pomodoro timer for 2 minutes"), "Expected start message not found")
+  end)
+
   it("can stop a pomodoro timer", function()
     tomata.setup({ duration = 1 })
     tomata.start()
